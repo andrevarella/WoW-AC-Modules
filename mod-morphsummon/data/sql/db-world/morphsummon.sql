dcbf35d4d7cf93e92d1d67aa34a5b2854f4d4ccc
andrevarella <andre.varella@hotmail.com>
2023-06-14 19:47:19 -0300

    morph summon

--- mod-morphsummon/data/sql/db-world/morphsummon.sql
+++ mod-morphsummon/data/sql/db-world/morphsummon.sql
@@ -14,7 +14,7 @@ SET @MENU_CHOICE     := @MENU_HELLO + 2;
 
 DELETE FROM `creature_template` WHERE `entry` = @ENTRY;
 INSERT INTO `creature_template` ( `entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`,`HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-( @ENTRY, 0, 0, 0, 0, 0, @MODELID, 0, 0, 0, @NAME, @SUBNAME, NULL, 0, 80, 80, 2, 35, 1, 1, 1.14286, 1, 0, 0, 1, 2000, 2000, 8, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 0, 0, 1, 0, 2, @SCRIPTNAME, 0);
+( @ENTRY, 0, 0, 0, 0, 0, @MODELID, 0, 0, 0, @NAME, @SUBNAME, NULL, 0, 80, 80, 2, 35, 0, 1, 1.14286, 1, 0, 0, 1, 2000, 2000, 8, 0, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 0, 0, 1, 0, 2, @SCRIPTNAME, 0);
 
 -- creatture_template_movement
 DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (@ENTRY);
