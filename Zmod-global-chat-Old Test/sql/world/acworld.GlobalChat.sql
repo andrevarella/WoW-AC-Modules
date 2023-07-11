77db83afda34c7d4062d329541004141fd4a45fc
andrevarella <andre.varella@hotmail.com>
2023-07-11 06:41:36 -0300

    update 11 07

--- Zmod-global-chat-Old Test/sql/world/acworld.GlobalChat.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `command` WHERE name IN ('chat');
-
-INSERT INTO `command` (`name`, `security`, `help`) VALUES 
-('chat', 0, 'Syntax: .chat $text\n.chat on To show Global Chat\n.chat off To hide Global Chat');
--- Zmod-global-chat-Old Test/src/loader.cpp
+++
@@ -1,5 +0,0 @@
-void AddSC_global_chat();
-
-void Addmod_global_chatScripts() {
-    AddSC_global_chat();
-}
