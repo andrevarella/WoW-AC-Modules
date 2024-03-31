0b1f02424671c6a7b72655075ccf6f532d88ab66
laasker <79991768+laasker@users.noreply.github.com>
2024-03-31 02:06:43 -0300

    asd

--- mod-npc-free-professions2/include.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-MOD_PROFESSION_NPC2_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-
-source $MOD_PROFESSION_NPC2L_ROOT"/conf/conf.sh.dist"
-
-if [ -f $MOD_PROFESSION_NPC2_ROOT"/conf/conf.sh" ]; then
-    source $MOD_PROFESSION_NPC2_ROOT"/conf/conf.sh"
-fi
